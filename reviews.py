89f10d670e6dd7022b1c25415dba83188be597d5
Matt Harvey <jmharv01@louisville.edu>
2024-09-16 13:25:08 -0700

    'Conducted exercise'

--- reviews.py
+++ reviews.py
@@ -1,2 +1,19 @@
 # add your code here
 
+import pandas as pd 
+
+reviews = pd.read_csv('data/winemag-data-130k-v2.csv')
+
+reviews.drop(columns=['Unnamed: 0'], inplace=True)
+
+function_dictionary = {'description':'count','points':'mean'}
+
+sorted = pd.DataFrame(reviews.groupby('country').aggregate(function_dictionary).sort_values(by='description', ascending=False))
+
+sorted = sorted.rename(columns={'description':'count'})
+
+sorted['points'] = sorted['points'].round(1)
+
+# sorted.to_csv('data/reviews_per_country.csv')
+
+print(sorted)
